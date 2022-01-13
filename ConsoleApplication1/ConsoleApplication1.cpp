21090901505d440343de405bccfd9f5b9e00895d
Alexander Tsyganok <Ciga_fu80@student.itstep.org>
2022-01-13 20:50:21 +0200

    Add binary tree(methods for adding and show element)

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,20 +1,68 @@
-﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+template <typename T>
+struct Node {
+    T value;
+    Node* left;
+    Node* right;
+    Node(T val) {
+       right = left = NULL;
+       value = val;
+   }
+};
+template <typename T>
+class BinTree {
+private:
+    Node<T>* first;
+    unsigned long size;
 
-#include <iostream>
+    void print(Node<T>* tmp, int l) {
+        while (tmp != NULL) {
+            print(tmp->left, l + 2);
+            for (int i = 1; i < l; i++) {
+                std::cout << " ";
+            }
+            std::cout << tmp->value << "\n";
+            tmp = tmp->right;
+            l += 2;
+        }
+    }
+
+    void insert(Node<T>** tmp,T val) {
+        if (*tmp == NULL) {
+           *tmp = new Node<T>(val);
+        }
+        else {
+            if ((*tmp)->value > val) {
+                insert((&(*tmp)->left), val);
+            }
+            if((*tmp)->value < val) {
+                insert((&(*tmp)->right), val);
+            }
+        }
+    }
+public:
+    explicit BinTree() {
+        this->first = NULL;
+        this->size = 0;
+    }
+    template <typename T>
+    void add(T val) {
+        insert(&this->first, val);
+    }
+    void printTree() {
+        print(this->first, 5);
+    }
+   
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    BinTree<int> num;
+    num.add(0);
+    num.add(1);
+    num.add(15);
+    num.add(18);
+    num.add(2);
+    num.add(-5);
+    num.printTree();
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
